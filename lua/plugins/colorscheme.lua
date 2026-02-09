ce84cbd6f247365b2132f891e02494edd09bed70
Luka <konnectwithpeter@gmail.com>
2026-02-09 22:49:32 +0300

    its so over

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
 }
